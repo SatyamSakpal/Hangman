121dec9c6cd39ed0609976e92d0738cb4d2c071a
Satyam Sakpal <sakpalsatyam3@gmail.com>
2023-11-08 18:45:41 +0530

    Main frontend functionality and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,54 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Keyboard from './Keyboard'
 import Word from './Word'
+import Gallow from "./Gallow";
 
 export default function Main() {
-    const wordToGuess = "CARROT"
+    const alphabets = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 
-    const createAlphaToGuessObj = () => {
+    const wordToGuess = "CARROT"
+    const [alphaToGuess , setAlphaToGuess] = useState(createAlphaToGuessObj())
+    const [keys, setKeys] = useState(createKeyArray())
+    const [ misses, setMisses ] = useState(0)
+    const [ won, setWon ] = useState(false)
+    const [ loss, setLoss ] = useState(false)
+    
+    function createAlphaToGuessObj() {
         let objectArr = []
         Array.from(wordToGuess).forEach(alpha => {
             objectArr.push({value: alpha, guessed: false})
         });
         return objectArr
     }
-
-    const [alphaToGuess , setAlphaToGuess] = useState(createAlphaToGuessObj())
-
     
-    const checkAlphabetExits = (e) => {
+    function createKeyArray() {
+        return alphabets.map(alphabet => {
+            return {
+                value: alphabet,
+                color: "#116FB2",
+                clicked: false
+            }
+        })
+    }
+    
+    function handKeyClick(e) {
         let clickedChar = e.target.textContent
-        let keyPos = e.target.dataset.pos
-        if(wordToGuess.includes(clickedChar)) {
-            setKeyColor(keyPos, "green")
-        } else {
-            setKeyColor(keyPos, "red")
-        }
+        let pos = Number.parseInt(e.target.dataset.pos)
+        
+        if(keys[pos].clicked || won || loss) 
+            return
+    
+        if(!wordToGuess.includes(clickedChar)) {
+            setKeyColor(pos, "red")
+            setMisses(misses => misses + 1)
+            return
+        } 
+        
+        setKeyColor(pos, "green")
+        updateAlphaToGuess(clickedChar)   
+    }
 
+    function updateAlphaToGuess(clickedChar) {
         setAlphaToGuess(prevAlphas => {
             return prevAlphas.map(char => {
                 if(char.value === clickedChar) {
@@ -33,37 +57,44 @@ export default function Main() {
                 return char
             })
         })
-    } 
+    }
     
-    const alphabets = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
-    
-    const createKeyArray = () => {
-        return alphabets.map(alphabet => {
-            return {
-                value: alphabet,
-                color: "#116FB2",
-            }
+    function setKeyColor(pos, colour) {
+        setKeys(prevKeys => {
+            return prevKeys.map((key, idx) => {
+                if(idx === pos)  {
+                    return {
+                        ...key,
+                        color: colour,
+                        clicked: true
+                    }
+                }
+                return key
+            })
         })
     }
+
+    useEffect(() => {
+        if( misses === 7 ) 
+        setLoss(loss => true)
+    }, [misses])
+        
+    useEffect(() => {
+        let wordGuessed = alphaToGuess.every(alpha => alpha.guessed)
+        if(wordGuessed) 
+            setWon(won => true)
+    }, [alphaToGuess])
     
-    const [keys, setKeys] = useState(createKeyArray())
-    
-    const setKeyColor = (pos, color) => {
-        let tempArr = keys
-        tempArr[pos].color = color
-        setKeys(tempArr)
-    }
-    
-    console.log(alphaToGuess)
+    console.log("lost",loss)
 
     return (
         <main>
             <div className='left-half'>
                 <Word alphaToGuess={alphaToGuess} />
-                <Keyboard keys={keys} checkAlphabetExits={checkAlphabetExits} />
+                <Keyboard keys={keys} checkAlphabetExits={handKeyClick} />
             </div>
             <div className='right-half'>
-                Gallow
+                <Gallow misses={misses} loss={loss} />
             </div>
         </main>
     )
---
+++ src/components/gallow_components/GallowStand.jsx
@@ -0,0 +1,12 @@
+export default function GallowStand() {
+    return (
+        <div className="gallowstand-container">
+            <div className="gallowstand head"></div>
+            <div className="gallowstand headsupport"></div>
+            <div className="gallowstand pole"></div>
+            <div className="gallowstand polesupport-left"></div>
+            <div className="gallowstand polesupport-right"></div>
+            <div className="gallowstand land"></div>
+        </div>
+    )
+}
