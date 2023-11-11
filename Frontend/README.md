e2dbae1af1613a815ca3d63bcadef8a46131a2ee
Satyam Sakpal <sakpalsatyam3@gmail.com>
2023-11-11 18:08:37 +0530

    game commit

--- Frontend/README.md
+++
@@ -1,3 +0,0 @@
-# Hangman
-
-A hangman game using react and an nodejs backend used as an api server .
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const {randomData} = require('./data/data')
 const app = express()
 
+app.use(express.static('./dist'))
+
 app.get("/api/randomWord", (req, res) => {
     res.json(randomData())
 })
